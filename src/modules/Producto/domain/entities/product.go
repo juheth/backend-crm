7ba2593182e39217e5de9f01ec1d98a12f77019a
juheth <juheth.ariza@gmail.com>
2025-06-22 16:39:23 -0500

    feature/obtener-todos-los-productos

--- src/modules/Producto/domain/entities/product.go
+++ src/modules/Producto/domain/entities/product.go
@@ -6,7 +6,7 @@ type Product struct {
 	ID          int       `json:"id"`
 	Name        string    `json:"name"`
 	Description string    `json:"description"`
-	Price       int       `json:"price"`
+	Price       float64   `json:"price"`
 	Stock       int       `json:"stock"`
 	Status      bool      `json:"status"`
 	CreatedAt   time.Time `json:"createdAt"`
